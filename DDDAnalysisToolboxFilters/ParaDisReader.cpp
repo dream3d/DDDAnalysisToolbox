10f6caf0ab553e336b3369dedf0d8f7b5fa7d9fa
Joey Kleingers <joey.kleingers@bluequartz.net>
2016-06-27 11:22:52 -0400

    Finishing updating FilterParameter constructors to new API. updates #549

--- DDDAnalysisToolboxFilters/ParaDisReader.cpp
+++ DDDAnalysisToolboxFilters/ParaDisReader.cpp
@@ -92,7 +92,7 @@ ParaDisReader::~ParaDisReader()
 void ParaDisReader::setupFilterParameters()
 {
   FilterParameterVector parameters;
-  parameters.push_back(InputFileFilterParameter::New("Input File", "InputFile", getInputFile(), FilterParameter::Parameter, "*", SIMPL_BIND_SETTER(ParaDisReader, this, InputFile), SIMPL_BIND_GETTER(ParaDisReader, this, InputFile)));
+  parameters.push_back(InputFileFilterParameter::New("Input File", "InputFile", getInputFile(), FilterParameter::Parameter, SIMPL_BIND_SETTER(ParaDisReader, this, InputFile), SIMPL_BIND_GETTER(ParaDisReader, this, InputFile), "*"));
   parameters.push_back(DoubleFilterParameter::New("Burgers Vector Length (Angstroms)", "BurgersVector", getBurgersVector(), FilterParameter::Parameter, SIMPL_BIND_SETTER(ParaDisReader, this, BurgersVector), SIMPL_BIND_GETTER(ParaDisReader, this, BurgersVector)));
 // parameters.push_back(SeparatorFilterParameter::New("Created Information", FilterParameter::Uncategorized));
   parameters.push_back(StringFilterParameter::New("Edge DataContainer Name", "EdgeDataContainerName", getEdgeDataContainerName(), FilterParameter::CreatedArray, SIMPL_BIND_SETTER(ParaDisReader, this, EdgeDataContainerName), SIMPL_BIND_GETTER(ParaDisReader, this, EdgeDataContainerName)));
