64a547efd7fabb7b89d8221ea14013cd0d9f2f26
Michael Jackson <mike.jackson@bluequartz.net>
2016-08-10 15:37:42 -0400

    clang format does not work well with comments after macros.

--- DDDAnalysisToolboxFilters/LocalDislocationDensityCalculator.h
+++ DDDAnalysisToolboxFilters/LocalDislocationDensityCalculator.h
@@ -60,7 +60,7 @@
  */
 class LocalDislocationDensityCalculator : public AbstractFilter
 {
-    Q_OBJECT /* Need this for Qt's signals and slots mechanism to work */
+    Q_OBJECT
   public:
     SIMPL_SHARED_POINTERS(LocalDislocationDensityCalculator)
     SIMPL_STATIC_NEW_MACRO(LocalDislocationDensityCalculator)
--- DDDAnalysisToolboxFilters/ParaDisReader.h
+++ DDDAnalysisToolboxFilters/ParaDisReader.h
@@ -61,7 +61,7 @@
  */
 class  ParaDisReader : public FileReader
 {
-    Q_OBJECT /* Need this for Qt's signals and slots mechanism to work */
+    Q_OBJECT
   public:
     SIMPL_SHARED_POINTERS(ParaDisReader)
     SIMPL_STATIC_NEW_MACRO(ParaDisReader)
